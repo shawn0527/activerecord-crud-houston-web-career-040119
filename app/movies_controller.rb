b4adb9a5ce1b70eea7f21dcd42bab32c2fd578b5
Shawn Ren <shawnren527@gmail.com>
2019-04-14 22:41:25 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -85,7 +85,7 @@ end
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
-  5.times do |i|
+  movie = 5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
 binding.pry
